bc50184e9cd7d4241536eb7b80fdebe07adbaaf1
0xhawk <hawk@horizonx.tech>
2025-03-11 21:05:11 +0400

    add imported

--- src/MultiSourceOracle.sol
+++ src/MultiSourceOracle.sol
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
+
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@pythnetwork/pyth-sdk-solidity/IPyth.sol";
 
 contract MultiSourceOracle {
 
