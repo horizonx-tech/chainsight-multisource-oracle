70593d514e69ea9d984be3857afed3d00e34e573
0xhawk <hawk@horizonx.tech>
2025-04-05 14:37:20 +0400

    version 0.8.20

--- src/MultiSourceOracle.sol
+++ src/MultiSourceOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import "./interface/IChainlink.sol";
